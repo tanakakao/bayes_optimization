2261fa8a363b1bdd3578dc1c8fa151daf0608f59
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-03-07 10:28:47 +0900

    Split classifier wrappers into dedicated module

--- models/classifier_gp/__init__.py
+++ models/classifier_gp/__init__.py
@@ -1,6 +1,11 @@
-from .models import ClassifierGPBinaryFromMulticlass, ClassifierMixedGPBinaryFromMulticlass, fit_classifier_mll
+from .binary_wrappers import (
+    ClassifierGPBinaryFromMulticlass,
+    ClassifierMixedGPBinaryFromMulticlass,
+)
+from .models import fit_classifier_mll
 
 __all__ = [
-    "ClassifierGPBinaryFromMulticlass", "ClassifierMixedGPBinaryFromMulticlass",
-    "fit_classifier_mll"
+    "ClassifierGPBinaryFromMulticlass",
+    "ClassifierMixedGPBinaryFromMulticlass",
+    "fit_classifier_mll",
 ]
